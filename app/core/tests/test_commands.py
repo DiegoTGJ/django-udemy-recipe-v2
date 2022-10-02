4d4b98d8a9d9123ced5f370150e2ab8180e62f09
Diego Tapia G <DiegoTGjobs@gmail.com>
2022-10-02 02:56:51 -0300

    modified tests.

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -14,6 +14,7 @@ from django.test import SimpleTestCase
 class CommandTests(SimpleTestCase):
     """Test commands."""
 
+    # noinspection PyMethodMayBeStatic
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database is not ready."""
         patched_check.return_value = True
@@ -22,9 +23,11 @@ class CommandTests(SimpleTestCase):
 
         patched_check.assert_called_once_with(databases=['default'])
 
+    # noinspection PyUnusedLocal
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError"""
+        # noinspection PyTypeChecker
         patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
 
